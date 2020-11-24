51a4bef540b601fabc9cb210dc2c65b6c92f295a
Danielv210796 <erik.vargas18@tectijuana.edu.mx>
2020-11-24 14:50:35 -0600

    conexiondbb

---
+++ Hospital/Hospital.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hospital.Web.Startup))]
+namespace Hospital.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
